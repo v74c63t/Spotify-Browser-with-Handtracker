346d68360721e4a19c33a970581fe9da3b9efa6b
v74c63t <vanesst5@uci.edu>
2023-02-13 21:26:28 -0800

    implemented sendRequestToExpress

--- client/src/app/services/spotify.service.ts
+++ client/src/app/services/spotify.service.ts
@@ -6,6 +6,7 @@ import { TrackData } from '../data/track-data';
 import { ResourceData } from '../data/resource-data';
 import { ProfileData } from '../data/profile-data';
 import { TrackFeature } from '../data/track-feature';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -22,7 +23,12 @@ export class SpotifyService {
     //Note: toPromise() is a deprecated function that will be removed in the future.
     //It's possible to do the assignment using lastValueFrom, but we recommend using toPromise() for now as we haven't
     //yet talked about Observables. https://indepth.dev/posts/1287/rxjs-heads-up-topromise-is-being-deprecated
-    return Promise.resolve();
+    //return Promise.resolve();
+    return lastValueFrom(this.http.get(this.expressBaseUrl + endpoint)). then ((response) => {
+      return response;
+    }, (err) => {
+      return err;
+    })
   }
 
   aboutMe():Promise<ProfileData> {
