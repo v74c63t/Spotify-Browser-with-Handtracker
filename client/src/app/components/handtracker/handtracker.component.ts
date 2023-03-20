eadb17d496e28cc040916d40fdc393257381670c
v74c63t <vanesst5@uci.edu>
2023-03-20 02:05:18 -0700

    Adjusted confidence

--- client/src/app/components/handtracker/handtracker.component.ts
+++ client/src/app/components/handtracker/handtracker.component.ts
@@ -30,7 +30,7 @@ export class HandtrackerComponent implements OnInit {
     flipHorizontal: true, // flip e.g for video
     maxNumBoxes: 20, // maximum number of boxes to detect
     iouThreshold: 0.5, // ioU threshold for non-max suppression
-    scoreThreshold: 0.75, // confidence threshold for predictions.
+    scoreThreshold: 0.85, // confidence threshold for predictions.
   };
 
   constructor() {
